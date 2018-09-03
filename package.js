75e17afcedd04887063f034674cb95ae52b861a4
Daniel Lamando <apt@danopia.net>
2018-09-02 20:52:09 -0700

    1.1.0: Patch for newer Meteor version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'danopia:google-api',
   summary: "A Meteor library to interact with Google's API",
-  version: '1.0.6',
+  version: '1.1.0',
   git: 'https://github.com/danopia/meteor-google-api'
 });
 
